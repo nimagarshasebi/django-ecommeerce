dec750095a6f883ca50ae1202138aef8b605016d
nima <nima.garshasebi9776@gmail.com>
2023-09-22 19:00:44 +0330

    make comment

--- ecommercecard/users/urls.py
+++ ecommercecard/users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import customerprofile,registercustomer,CustomerLogin,make_user_address,showaddress,set_default_address
+from .views import customerprofile,registercustomer,CustomerLogin,make_user_address,showaddress
 from django.contrib.auth import views as auth_views
 
 
@@ -10,5 +10,5 @@ urlpatterns = [
     path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
     path('add-address/',make_user_address,name='add-address'),
     path('address/',showaddress,name='address'),
-    path('bbb/',set_default_address,name='bbb')
+    #path('bbb/',set_default_address,name='bbb')
 ]
