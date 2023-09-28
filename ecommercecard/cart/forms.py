b1229153a2b53fcbfb5707eebfe2d85fbac495ff
nima <nima.garshasebi9776@gmail.com>
2023-09-28 09:48:51 +0330

    change cartform and make it better

--- ecommercecard/cart/forms.py
+++ ecommercecard/cart/forms.py
@@ -1,7 +1,7 @@
 from django import forms
-PRODUCT_COUNT_CHOICES=[(i,str(i)) for i in range(1,9)]
+
 class CartAddProductForm(forms.Form):
-    product_count=forms.TypedChoiceField(choices=PRODUCT_COUNT_CHOICES,coerce=int,label="",widget=forms.Select(attrs={'class': 'input-number__input form-control form-control-lg'}))
+    product_count=forms.IntegerField(min_value=1,label="",initial=1,widget=forms.NumberInput(attrs={'class':'product-count'}))
     update=forms.BooleanField(required=False,initial=False,widget=forms.HiddenInput())
 
 
